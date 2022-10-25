4eb585cc097c63a9094ea7a325fed0c49a4a7f9d
AlinaCDevelopment <alina@development.pt>
2022-10-25 16:29:50 +0100

    Implemented Sliders

--- lib/screens/box_hide_screen.dart
+++ lib/screens/box_hide_screen.dart
@@ -3,65 +3,77 @@ import 'package:hooks_animations/animations/box_hide.dart';
 import 'package:hooks_animations/services/image_service.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+const _minHeight = 100.1;
+const _maxHeight = 500.1;
+
+const _minDuration = 0.1;
+const _maxDuration = 5.0;
+
 class BoxHideScreen extends ConsumerWidget {
   BoxHideScreen({super.key});
-  final _heightProvider = StateProvider<double>(
-    (ref) => 0.1,
+  final heightProvider = StateProvider<double>(
+    (ref) => _minHeight,
   );
-  final _durationProvider = StateProvider<double>(
-    (ref) => 0.1,
+  final durationProvider = StateProvider<double>(
+    (ref) => _minDuration,
   );
-  final _pickerImageProvider = StateProvider<Image?>(
+  final pickerImageProvider = StateProvider<Image?>(
     (ref) => null,
   );
-  final _hiderImageProvider = StateProvider<Image?>(
+  final hiderImageProvider = StateProvider<Image?>(
     (ref) => null,
   );
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final hider = ref.watch(_hiderImageProvider.state).state;
-    final picker = ref.watch(_pickerImageProvider.state).state;
+    final hider = ref.watch(hiderImageProvider.state).state;
+    final picker = ref.watch(pickerImageProvider.state).state;
 
     return Column(
       children: [
         Slider(
             label: 'Height',
-            value: ref.watch(_heightProvider.state).state,
-            max: 3.0,
-            min: 0.1,
+            value: ref.watch(heightProvider.state).state,
+            max: _maxHeight,
+            min: _minHeight,
             onChanged: (value) {
-              ref.read(_heightProvider.notifier).state = value;
+              ref.read(heightProvider.notifier).state = value;
             }),
+        Text(ref.watch(heightProvider.state).state.toString()),
         Slider(
             label: 'Duration',
-            value: ref.watch(_durationProvider.state).state,
-            max: 5.0,
-            min: 0.1,
+            value: ref.watch(durationProvider.state).state,
+            max: _maxDuration,
+            min: _minDuration,
             onChanged: (value) {
-              ref.read(_durationProvider.notifier).state = value;
+              ref.read(durationProvider.notifier).state = value;
             }),
+        Text(ref.watch(durationProvider.state).state.toString()),
         TextButton(
             onPressed: () async {
               final imageFile = await getImageFromGallery();
-              ref.read(_pickerImageProvider.notifier).state =
+              ref.read(pickerImageProvider.notifier).state =
                   imageFile != null ? Image.file(imageFile) : null;
             },
             child: const Text('Pick the picker')),
         TextButton(
             onPressed: () async {
               final imageFile = await getImageFromGallery();
-              ref.read(_hiderImageProvider.notifier).state =
+              ref.read(hiderImageProvider.notifier).state =
                   imageFile != null ? Image.file(imageFile) : null;
             },
             child: const Text('Pick the hider')),
         if (hider != null && picker != null)
-          BoxHide(
-            pickerImage: picker!,
-            hiderImage: hider!,
-            animationHeight: ref.read(_heightProvider.notifier).state,
-            animationSeconds:
-                ref.read(_durationProvider.notifier).state.round(),
+          Expanded(
+            child: Center(
+              child: BoxHide(
+                pickerImage: picker!,
+                hiderImage: hider!,
+                animationHeight: ref.read(heightProvider.notifier).state,
+                animationSeconds:
+                    ref.read(durationProvider.notifier).state.round(),
+              ),
+            ),
           )
       ],
     );
